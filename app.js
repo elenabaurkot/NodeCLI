04f7bad421c0ac49adc2d3ae3e254b4cbd3057a2
elenabaurkot <elenabaurkot@gmail.com>
2020-02-20 23:10:51 -0500

    jumbotron on main.html created

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 var fs = require("fs");
 var inquirer = require("inquirer");
 
+
+inquirer
+    .prompt([
+
+    ])
+    .then (answers => {
+        
+    })
+
+// going to need prompts that at the end write to team.html under outputs
+
+// going to need switch case based on which type of employee the user selects 
+
+// need to create html templates for each user 
+
+// the main should have all of the code 
+
+// handlebars with spaces for placeholders
+
+//  In your HTML template files, you may want to add a placeholder character that helps your program identify where the dynamic markup begins and ends.
+
+// The project must prompt the user to build an engineering team. An engineering team consists of a manager, and any number of engineers and interns.
